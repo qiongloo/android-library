a7355547787fed26232440774f332d8c40237fd3
yangfeng <yangfeng40@126.com>
2014-11-14 11:26:58 +0800

    修改新的版本

--- pub_library_samply/src/com/example/pub_library_smaply/MainActivity.java
+++ pub_library_samply/src/com/example/pub_library_smaply/MainActivity.java
@@ -4,22 +4,47 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.qiongloo.view.annotation.xtuils.ViewInject;
 
 public class MainActivity extends Activity {
-
+	
+	@ViewInject(R.id.swiptList_click)
+	private Button swiptList_click;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initlistiner();
 	}
 	
 	/**
-	 * swiptList点击事件
-	 * @param v
+	 * 点击事件
 	 */
-	public void handleSample1(View v) {
-		startActivity(new Intent(this, SwiptListviewActivity.class));
+	private void initlistiner(){
+	
+		/**
+		 * swiptList展示
+		 * @param v
+		 */
+		swiptList_click.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				MainActivity.this.startActivity(new Intent(MainActivity.this, SwiptListviewActivity.class));
+			}
+		});
+			
+
+	
+	
 	}
+	
+	
+
 
 
 	
